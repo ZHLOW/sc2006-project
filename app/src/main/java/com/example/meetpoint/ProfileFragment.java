a8affcaee7df60929402af61120de7c91b87c1a7
terenceohyz <104288514+terenceohyz@users.noreply.github.com>
2023-03-31 17:34:37 +0800

    Added Profile Picture

--- app/src/main/java/com/example/meetpoint/ProfileFragment.java
+++ app/src/main/java/com/example/meetpoint/ProfileFragment.java
@@ -1,7 +1,9 @@
 package com.example.meetpoint;
 
+import android.widget.Toast;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,6 +11,8 @@ import android.view.ViewGroup;
 import androidx.activity.OnBackPressedCallback;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -19,10 +23,23 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import android.Manifest;
+import android.net.Uri;
+import android.provider.MediaStore;
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 public class ProfileFragment extends Fragment {
 
     private FirebaseAuth authProfile;
+    private static final int PICK_IMAGE_REQUEST = 1;
+    private static final int REQUEST_STORAGE_PERMISSION = 2;
     FirebaseUser user;
     ImageView imageView;
     private TextView textViewUsername, textViewFullName, textViewEmail,  textViewMobile , textViewUniqueID, textViewLocation;
@@ -42,6 +59,13 @@ public class ProfileFragment extends Fragment {
         textViewMobile = view.findViewById(R.id.textView_show_mobileNumber);
         textViewLocation = view.findViewById(R.id.textView_show_location);
         authProfile = FirebaseAuth.getInstance();
+        imageView = view.findViewById(R.id.imageViewProfileDP);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                requestStoragePermissionAndOpenImagePicker();
+            }
+        });
 
         FirebaseUser firebaseUser = authProfile.getCurrentUser();
         String userID = firebaseUser.getUid();
@@ -72,5 +96,74 @@ public class ProfileFragment extends Fragment {
 
         return view;
     }
+    private void requestStoragePermissionAndOpenImagePicker() {
+        if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(requireActivity(), new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_STORAGE_PERMISSION);
+        } else {
+            openImagePicker();
+        }
+    }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_STORAGE_PERMISSION && grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            openImagePicker();
+        } else {
+            Toast.makeText(requireContext(), "Permission denied", Toast.LENGTH_SHORT).show();
+        }
+    }
+    private void openImagePicker() {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, PICK_IMAGE_REQUEST);
+    }
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == Activity.RESULT_OK && data != null && data.getData() != null) {
+            Uri imageUri = data.getData();
+            imageView.setImageURI(imageUri);
+            uploadImageToFirebaseStorage(imageUri);
+        }
+    }
+    private void uploadImageToFirebaseStorage(Uri imageUri) {
+        StorageReference storageReference = FirebaseStorage.getInstance().getReference("profile_pictures").child(user.getUid());
+
+        storageReference.putFile(imageUri).continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+            @Override
+            public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                if (!task.isSuccessful()) {
+                    throw task.getException();
+                }
+                return storageReference.getDownloadUrl();
+            }
+        }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+            @Override
+            public void onComplete(@NonNull Task<Uri> task) {
+                if (task.isSuccessful()) {
+                    Uri downloadUri = task.getResult();
+                    updateUserProfilePhoto(downloadUri);
+                } else {
+                    Toast.makeText(requireContext(), "Upload failed: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void updateUserProfilePhoto(Uri photoUri) {
+        DatabaseReference userRef = FirebaseDatabase.getInstance().getReference("Users").child(user.getUid());
+        userRef.child("profilePhotoUrl").setValue(photoUri.toString())
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Toast.makeText(requireContext(), "Profile photo updated successfully", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(requireContext(), "Failed to update profile photo: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
 }
